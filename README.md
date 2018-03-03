12f7620836c61fa1bae8c42344012b6baa28455d
penguinwan <penguin_wan@yahoo.com.sg>
2018-03-03 10:14:46 +0100

    implemented bean created from spring application context.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# Introduction
+Find detailed explanation in [here](https://github.com/goeuro/challenges/tree/master/bus_route_challenge)
 
+# How to
+- sample data attached, run the spring boot program by `java -Ddata.file=sample-data -jar bus-route-1.0-SNAPSHOT.jar`
+- request url by `http://localhost:8088/api/direct?dep_sid=2&arr_sid=5`
--- src/main/java/com/penguinwan/infrastructure/web/Application.java
+++ src/main/java/com/penguinwan/infrastructure/web/Application.java
@@ -1,10 +1,36 @@
 package com.penguinwan.infrastructure.web;
 
+import com.penguinwan.domain.IDataProvider;
+import com.penguinwan.domain.IRouteFinder;
+import com.penguinwan.domain.SimpleRouteFinder;
+import com.penguinwan.infrastructure.FileBasedDataProvider;
+import com.penguinwan.infrastructure.InvalidFileException;
+import com.penguinwan.infrastructure.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class Application {
+
+    @Value("${data.file}")
+    private String dataFileLocation;
+
+    @Bean
+    public IRouteFinder routeFinder(IDataProvider dataProvider) {
+        return new SimpleRouteFinder(dataProvider);
+    }
+
+    @Bean
+    public IDataProvider dataProvider()
+            throws InvalidFormatException, InvalidFileException {
+        Path dataFilePath = Paths.get(dataFileLocation);
+        return new FileBasedDataProvider(dataFilePath);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
